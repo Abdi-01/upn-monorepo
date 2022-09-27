2d5802ceb061a096b72da73d4d6824ac2b3e1182
hasbifadillah <hasbifadillah@outlook.com>
2022-09-27 16:56:30 +0700

    feat: Implement express in server app

--- projects/server/index.js
+++ projects/server/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World !");
+import express from "express";
+import cors from "cors";
+
+const port = 8000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.send("Hello world !");
+});
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log(`App running on http://localhost:${port}`);
+    return;
+  }
+
+  console.log("Error", err);
+});
